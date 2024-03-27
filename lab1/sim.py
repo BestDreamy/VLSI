5bd9d402cce335219be877bb5a045aec21ed6636
vlsi <3362484152@qq.com>
2024-03-27 21:49:37 +0800

    Update

--- lab1/sim.py
+++ lab1/sim.py
@@ -1,14 +1,20 @@
 import subprocess as sp
 import sys
-
+import os
 
 def run_sim():
 	flag = " -full64 -kdb -debug_acc+r "
 	vsrc = " multiplier_tb.v multiplier.v half_adder.v full_adder.v "  
+	path = os.path.dirname(os.path.abspath(__file__))
+	for filename in os.listdir(path):
+		if filename.endswith('.fsdb'):
+			wave = filename
 
 	sim_cmd = [
 		'echo vcs running',
-		# f'vcs {vsrc} {flag}'
+		f'vcs {vsrc} {flag}',
+		'./simv',
+		f'verdi {wave}',
 	]
 
 	for cmd in sim_cmd:
@@ -21,6 +27,5 @@ if __name__ == '__main__':
 	else: 
 		arg = sys.argv[1]
 
-	print(arg)
 	if arg == "sim" or arg == "default":
-		run_sim()	
+		run_sim()
